a07f140961db0f0ab80d91152d80b45bccf8f348
Ben Abdallah Anwer <anwer8322@gmail.com>
2020-11-26 12:33:57 +0100

    je termine l'implèmentation de interface login et signup avec leurs liens de nagivation entre les activitès ( login , signup )

--- app/src/main/java/com/example/isammcom/LoginActivity.java
+++ app/src/main/java/com/example/isammcom/LoginActivity.java
@@ -2,13 +2,25 @@ package com.example.isammcom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+     TextView inscriretv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Passage LoginActivity ---->SignupActivity a partir textview (S'inscrire)
+        inscriretv = findViewById(R.id.inscriretv);
+        inscriretv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,SignupActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/isammcom/StartActivity.java
+++ app/src/main/java/com/example/isammcom/StartActivity.java
@@ -2,13 +2,36 @@ package com.example.isammcom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+    Button connectez ;
+    Button inscrire ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        connectez = findViewById(R.id.connectez);
+        inscrire = findViewById(R.id.inscrire);
+
+        //Passage StartActivity ----> Activity_login
+        connectez.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             startActivity(new Intent(StartActivity.this,LoginActivity.class));
+            }
+        });
+
+        inscrire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this,SignupActivity.class));
+            }
+        });
     }
 }
