a6ba47eb46b16fedf8d52e1171c7b781dcfc60d3
Ben Abdallah Anwer <anwer8322@gmail.com>
2020-11-26 17:59:22 +0100

    je fais l'animation de logo dans SplashActivity

--- app/src/main/java/com/example/isammcom/SplashActivity.java
+++ app/src/main/java/com/example/isammcom/SplashActivity.java
@@ -5,13 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
     final int SPLASH_TIME_OUT = 5000;
+    Animation topanim ;
+    ImageView logo ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //Animation de logo
+        topanim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        logo=findViewById(R.id.logo);
+        logo.setAnimation(topanim);
 
         new Handler().postDelayed(new Runnable() {
             @Override
