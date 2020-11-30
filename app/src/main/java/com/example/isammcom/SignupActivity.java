d0c1a5f02fafa8fd02fa7cc36d559e7dbb831ad6
oueslatimohamed <oueslatimohamed01@gmail.com>
2020-11-30 19:27:14 +0100

    I fixed FireBase connexion (signIn and signUp ) with verification email

--- app/src/main/java/com/example/isammcom/SignupActivity.java
+++ app/src/main/java/com/example/isammcom/SignupActivity.java
@@ -17,6 +17,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -57,7 +58,7 @@ public class SignupActivity extends AppCompatActivity {
                String str_password = mPassword.getText().toString();
 
                if(TextUtils.isEmpty(str_name) || TextUtils.isEmpty(str_username) || TextUtils.isEmpty(str_email) || TextUtils.isEmpty(str_password)){
-
+                   pd.dismiss();
                    Toast.makeText(SignupActivity.this,"Remplir tout les champs !", Toast.LENGTH_SHORT).show();
 
                } else {
@@ -92,7 +93,7 @@ public class SignupActivity extends AppCompatActivity {
                                     @Override
                                     public void onComplete(@NonNull Task<Void> task) {
                                         if(task.isSuccessful()){
-                                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                                            final FirebaseUser firebaseUser = auth.getCurrentUser();
                                             String studid = firebaseUser.getUid();
 
                                             DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Students").child(studid);
@@ -110,16 +111,21 @@ public class SignupActivity extends AppCompatActivity {
                                                 @Override
                                                 public void onComplete(@NonNull Task<Void> task) {
                                                     if(task.isSuccessful()){
-                                                        pd.dismiss();
-                                                        Intent intent = new Intent(SignupActivity.this , LoginActivity.class);
-                                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                                                        startActivity(intent);
+                                                       if(email.equals(firebaseUser.getEmail().trim())){
+                                                           pd.dismiss();
+                                                           Toast.makeText(SignupActivity.this , "email is already exist ",Toast.LENGTH_SHORT).show();
+                                                       } else {
+                                                           pd.dismiss();
+                                                           Intent intent = new Intent(SignupActivity.this , LoginActivity.class);
+                                                           intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                                           startActivity(intent);
+                                                       }
                                                     }
                                                 }
                                             });
                                         } else {
                                             pd.dismiss();
-                                            Toast.makeText(SignupActivity.this , "You can't register",Toast.LENGTH_SHORT).show();
+                                            Toast.makeText(SignupActivity.this , "email is already exist",Toast.LENGTH_SHORT).show();
                                         }
                                     }
                                 });
